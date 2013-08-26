77dafd1888d53b8240866ef33d41daf5c264a22e
nabedge <nabedge@gmail.com>
2013-08-26 21:39:21 +0900

    add test.

--- src/test/java/com/example/SimpleTest.java
+++ src/test/java/com/example/SimpleTest.java
@@ -11,7 +11,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.example.qdslsample.meta.Mytable;
 import com.example.qdslsample.meta.QMytable;
+import com.mysema.query.Tuple;
 import com.mysema.query.sql.SQLQuery;
+import com.mysema.query.types.MappingProjection;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:applicationContext.xml" })
@@ -24,7 +26,7 @@ public class SimpleTest {
 
     @Test
     public void queryDslSample01() {
-        logger.info("******** START ********");
+        logger.info("******** queryDslSample01 START ********");
         SQLQuery query = qdslTemplate.newSqlQuery();
         QMytable qMytable = QMytable.mytable;
         query.from(qMytable).where(qMytable.id.eq(Long.parseLong("1")));
@@ -32,7 +34,40 @@ public class SimpleTest {
 
         // SQL statement will be executed.
         // But I can not get the resultSet as Mytable instance !
-        logger.info("******** {}", mytable.getMyarray().toString());
+        logger.info("******** queryDslSample01 END {}", mytable.getMyarray()
+                .toString());
+    }
+
+    @Test
+    public void queryDslSample02() {
+        logger.info("******** START ********");
+        SQLQuery query = qdslTemplate.newSqlQuery();
+        QMytable qMytable = QMytable.mytable;
+        query.from(qMytable).where(qMytable.id.eq(Long.parseLong("1")));
+        Mytable mytable = qdslTemplate.queryForObject(query,
+                new MappingMytableProjection(qMytable));
+        logger.info("******** queryDslSample02 END {}", mytable.getMyarray()
+                .toString());
+    }
+
+    private static class MappingMytableProjection extends
+            MappingProjection<Mytable> {
+
+        private static final long serialVersionUID = 1L;
+        private static final QMytable qMytable = QMytable.mytable;
+
+        public MappingMytableProjection(QMytable qMytable) {
+            super(Mytable.class, qMytable.id, qMytable.myarray);
+        }
+
+        @Override
+        protected Mytable map(Tuple tuple) {
+            Mytable mytable = new Mytable();
+            mytable.setId(tuple.get(qMytable.id));
+            mytable.setMyarray(tuple.get(qMytable.myarray));
+            return mytable;
+        }
+
     }
 
 }
